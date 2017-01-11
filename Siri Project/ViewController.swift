9eec54a499f5b38e883472de2d1f7e6ee1a7a3a1
cstark <student@cstark.local>
2017-01-11 09:46:55 -0600

    Initial Commit

---
+++ Siri Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Siri Project
+//
+//  Created by Jose Ramon Soto on 1/11/17.
+//  Copyright © 2017 Jose Ramon Soto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
